1055fa7e9925cef2b8a881647a2876aba497dc3c
Shivanshi Tyagi <shivanshityagi@icloud.com>
2022-07-13 23:09:16 +0530

    update docs link
    Signed-off-by: Shivanshi Tyagi <shivanshityagi@icloud.com>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -37,7 +37,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-          'https://github.com/lexdao/lexlib-sitetree/main',
+          'https://github.com/lexDAO/lexlib-site/blob/main/',
         },
         blog: {
           showReadingTime: true,
